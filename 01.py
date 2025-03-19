56d99ad5a0462641dd860929e6f49c4ea8d6f5fa
redwhale <graveofwhale@gmail.com>
2025-03-19 09:13:04 +0900

    2 try

--- 01.py
+++ 01.py
@@ -15,3 +15,12 @@ for mark in marks:   # 90, 25, 67, 45, 80을 순서대로 mark에 대입
         print("%d번 학생은 합격입니다." % number)
     else: 
         print("%d번 학생은 불합격입니다." % number)
+
+marks = [90, 25, 67, 45, 80]
+
+number = 0 
+for mark in marks: 
+    number = number +1 
+    if mark < 60:
+        continue 
+    print("%d번 학생 축하합니다. 합격입니다. " % number)
