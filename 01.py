c4d09061c77b7732686c073bb56fe2c40fd6e8c8
fractal mind <fractal9minds@gmail.com>
2025-03-19 09:04:27 +0900

    3번째 ~~

--- 01.py
+++ 01.py
@@ -5,4 +5,12 @@ test_arr = [1, 2, 3, 4, 5]
 for i in test_arr:
     print(i)
 
-    
+marks = [90, 25, 67, 45, 80]   # 학생들의 시험 점수 리스트
+
+number = 0   # 학생에게 붙여 줄 번호
+for mark in marks:   # 90, 25, 67, 45, 80을 순서대로 mark에 대입
+    number = number +1 
+    if mark >= 60: 
+        print("%d번 학생은 합격입니다." % number)
+    else: 
+        print("%d번 학생은 불합격입니다." % number)
